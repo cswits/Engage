d4e957f3d3e44b14b039925764014699db83d002
Jose Ghislain Quenum <josum@me.com>
2013-02-19 21:56:01 +0200

    Changing data handler into a singleton

--- server/lib/handlers/data-handler.js
+++ server/lib/handlers/data-handler.js
@@ -1,27 +1,32 @@
 var mongo = require('mongojs');
 
-exports.DataHandler = (function () {
-	function DataHandler(port, host) {
-		console.log("Creating a data handler object with database on port %d and host %s", port, host);
-		this.port = port;
-		this.host = host;
-		this.dbUrl = "engageDB";
-		this.db = mongo.connect(this.dbUrl);
+exports.DataHandler = (function() {
+	this.dataHandlerInstance = null;
+	
+	var getInstance = function(port, host) {
+		if (!this.dataHandlerInstance) {
+			this.dataHandlerInstance = createInstance(port, host);
+		}
 		
-		// finding data from the database
-		DataHandler.prototype.findData = function(bucketName, criteria, callback) {
-			this.db[bucketName].find(criteria, function(findError, findResult) {
-				callback(findError, findResult);
-			});
-		};
-		
-		// Adding data to the database
-		DataHandler.prototype.saveData = function(bucketName, data, callback) {
-			this.db[bucketName].save(data, function(saveError, saveResult) {
-				callback(saveError, saveResult);
-			});
-		};
-		
+		return this.dataHandlerInstance;
 	}
-	return DataHandler;
+	
+	var createInstance = function(dbPort, dbHost) {
+		var port = dbPort;
+		var host = dbHost;
+		var dbUrl = "engageDB";
+		var db = mongo.connect(dbUrl);
+		return {
+			findData: function(bucketName, criteria, callback) {
+				db[bucketName].find(criteria, function(findError, findResult) {
+					callback(findError, findResult);
+				});
+			},
+			saveData: function(bucketName, data, callback) {
+				db[bucketName].save(data, function(saveError, saveResult){
+					callback(saveError, saveResult);
+				});
+			}
+		}
+	}
 })();
