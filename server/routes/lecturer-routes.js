aabc4ef69c9311b1234b7951f75fce2cabd53eb7
Jose Ghislain Quenum <josum@me.com>
2013-02-19 17:16:20 +0200

    cleaned up the lecturer routing file

--- server/routes/lecturer-routes.js
+++ server/routes/lecturer-routes.js
@@ -4,53 +4,18 @@ var lecturer = new Lecturer();
 var LecturerController = requires('../lib/controllers/lecturer-controller').LecturerController;
 
 module.exports = function(app) {
+	// login
 	app.get('/lecturers/login', function(request, response) {
 		new LecturerController().authenticateLecturer(request, response);
 	});
 	
+	// create a lecturer in the system
 	app.get('/lecturers/create', function(request, response) {
 		new LecturerController().createLecturer(request, response);
 	});
 	
+	// delete a lecturer from the system
 	app.get('/lecturers/delete/:username', function(request, response) {
 		new LecturerController().deleteLecturer(request, response);
 	});
 }
-
-
-// lecturer logging into the system
-app.get('/lecturers/login', function(request, response) {
-	console.log("Lecturer login to Engage ...");
-	var lecturerUsername =  request.body["username"];
-	var lecturerPassword = request.body["password"];
-	lecturer.authenticate(lecturerUsername, lecturerPassword, function(authenticationError, result) {
-		if (authenticationError) this.respondWithError(authenticationError, response);
-		else this.respondWithSuccess(result, request, response);
-	});
-});
-
-app.get('/lecturers/create', function(request, response) {
-	console.log("Creating a lecturer in the db ...");
-	
-	var username = request.body["username"];
-	var password = request.body["password"];
-	var lastname = request.body["lastname"];
-	var firstname = request.body["firstname"];
-	var title = request.body["title"];
-	
-	lecturer.create(username, password, lastname, firstname, title, function(createError, createResult) {
-		if (createError) this.respondWithError(createError, response);
-		else this.respondWithSuccess(createResult, request, response);
-	});
-});
-
-app.get('/lecturers/delete/:username', function(request, response) {
-	console.log("Deleting user " + request.params["username"] + ' ...');
-	
-	var username = request.params["username"];
-	
-	lecturer.delete(username, function(deleteError, deleteResult) {
-		if (deleteError) this.respondWithError(deleteError, response);
-		else this.respondWithSuccess(deleteResult, request, response);
-	});
-});
