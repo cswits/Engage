0dd627fb79750f14237786e8cb65587175ddb653
Jose Ghislain Quenum <josum@me.com>
2013-02-25 12:32:59 +0200

    Made lecture controller jslint compatible

--- server/lib/controllers/lecture-controller.js
+++ server/lib/controllers/lecture-controller.js
@@ -1,64 +1,69 @@
+// lecture-controller.js
+"use strict";
+
 var Lecture = require('../models/lecture').Lecture;
 var ResponseHandler = require('../handlers/response-handler').ResponseHandler;
 
 exports.LectureController = (function(){
 	function LectureController() {
-		this.prototype.lecture = new Lecture(27017, "localhost");
-		this.prototype.responseHandler = ResponseHandler.getInstance();
-		
-		LectureController.prototype.createLectureCode = function(request, response) {
-			console.log("Generating a new lecture code for course...");
-			
-			var courseCode = request.body["courseCode"];
-			var lecturerUsername = request.body["username"];
-			
-			this.lecture.getNewLectureCode(courseCode, lecturerUsername, function(newCourseCodeError, newCourseCodeResult){
-				this.responseHandler.handleResponse(newCourseCodeError, newCourseCodeResult, request, response);
-			});
-		};
-		
-		LectureController.prototype.joinLecture = function(request, response) {
-			console.log("Student joining a lecture...");
-			
-			var lectureCode = request.body["lectureCode"];
-			var deviceId = request.body["deviceId"];
-			
-			this.lecture.joinLecture(lectureCode, devideId, function(joinLectureError, joinLectureResult){
-				this.responseHandler.handleResponse(joinLectureError, joinLectureResult, request, response);
-			});	
-		};
-		
-		LectureController.prototype.endLecture = function(request, response) {
-			console.log("Lecturer ending a lecture...");
-			
-			var lectureCode = request.body["lectureCode"];
-			
-			this.lecture.endLecture(lectureCode, function(endLectureError, endLectureResult){
-				this.responseHandler.handleResponse(endLectureError, endLectureResult, request, response);
-			});
-		};
-		
-		LectureController.prototype.leaveLecture = function(request, response) {
-			console.log("Student leaving lecture...");
-			
-			var lectureCode = request.body["lectureCode"];
-			var deviceId = request.body["deviceId"];
-			this.lecture.leaveLecture(lectureCode, deviceId, function(leaveLectureError, leaveLectureResult){
-				this.responseHandler.handleResponse(leaveLectureError, leaveLectureResult, request, respponse);
-			});
-		};
-		
-		LectureController.prototype.submitUnderstandingLevel = function(request, response) {
-			console.log("Student submitting their current level of understanding...");
-			
-			var lectureCode = request.body["lectureCode"];
-			var deviceId = request.body["deviceId"];
-			var understandingLevel = request.body["understandingLevel"];
-			
-			this.lecture.submitUnderstandingLevel(lectureCode, deviceId, understandingLevel, function(understandingError, understandingResult){
-				this.responseHandler.handleResponse(understandingError, understandingResult, request, response);
-			});
-		};
+		this.lecture = new Lecture(27017, "localhost");
+		this.responseHandler = ResponseHandler.getInstance();
 	}
+
+	LectureController.prototype.createLectureCode = function(request, response) {
+		console.log("Generating a new lecture code for course...");
+			
+		var courseCode = request.body.courseCode;
+		var lecturerUsername = request.body.username;
+			
+		this.lecture.getNewLectureCode(courseCode, lecturerUsername, function(newCourseCodeError, newCourseCodeResult){
+			this.responseHandler.handleResponse(newCourseCodeError, newCourseCodeResult, request, response);
+		});
+	};
+
+	LectureController.prototype.joinLecture = function(request, response) {
+		console.log("Student joining a lecture...");
+			
+		var lectureCode = request.body.lectureCode;
+		var deviceId = request.body.deviceId;
+			
+		this.lecture.joinLecture(lectureCode, deviceId, function(joinLectureError, joinLectureResult){
+			this.responseHandler.handleResponse(joinLectureError, joinLectureResult, request, response);
+		});
+	};
+
+	LectureController.prototype.endLecture = function(request, response) {
+		console.log("Lecturer ending a lecture...");
+			
+		var lectureCode = request.body.lectureCode;
+			
+		this.lecture.endLecture(lectureCode, function(endLectureError, endLectureResult){
+			this.responseHandler.handleResponse(endLectureError, endLectureResult, request, response);
+		});
+	};
+
+	LectureController.prototype.leaveLecture = function(request, response) {
+		console.log("Student leaving lecture...");
+			
+		var lectureCode = request.body.lectureCode;
+		var deviceId = request.body.deviceId;
+
+		this.lecture.leaveLecture(lectureCode, deviceId, function(leaveLectureError, leaveLectureResult){
+			this.responseHandler.handleResponse(leaveLectureError, leaveLectureResult, request, response);
+		});
+	};
+
+	LectureController.prototype.submitUnderstandingLevel = function(request, response) {
+		console.log("Student submitting their current level of understanding...");
+			
+		var lectureCode = request.body.lectureCode;
+		var deviceId = request.body.deviceId;
+		var understandingLevel = request.body.understandingLevel;
+			
+		this.lecture.submitUnderstandingLevel(lectureCode, deviceId, understandingLevel, function(understandingError, understandingResult){
+			this.responseHandler.handleResponse(understandingError, understandingResult, request, response);
+		});
+	};
+
 	return LectureController;
 })();
