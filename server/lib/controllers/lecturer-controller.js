e8d3fe4cf226ac60b56a281b6127cd5d47f2da2a
Jose Ghislain Quenum <josum@me.com>
2013-02-25 12:18:32 +0200

    Made lecturer controller jslint compatible

--- server/lib/controllers/lecturer-controller.js
+++ server/lib/controllers/lecturer-controller.js
@@ -1,43 +1,49 @@
+// lecturer-controller.js
+"use strict";
+
 var Lecturer = require('../models/lecturer').Lecturer;
 var ResponseHandler = require('../handlers/response-handler').ResponseHandler;
 
-exports.LecturerController = (function() {
+exports.LecturerController = (function(){
 	function LecturerController(io) {
-		this.prototype.lecturer = new Lecturer(27017, "localhost", io);
-		this.prototype.responseHandler = ResponseHandler.getInstance();
-		
-		LecturerController.prototype.authenticateLecturer = function(request, response) {
-			console.log("Lecturer logging into the system...");
-			
-			var lecturerUsername = request.body["username"];
-			var lecturerPassword = request.body["password"];
-			this.lecturer.authenticate(lecturerUsername, lecturerPassword, function(authenticationError, authenticationResult) {
-				this.responseHandler.handleResponse(authenticationError, authenticationResult, request, response);
-			});
-		};
-		
-		LecturerController.prototype.createLecturer = function(request, response) {
-			console.log("Creating a new lecturer into the system...");
-			
-			var username = request.body["username"];
-			var password = request.body["password"];
-			var lastname = request.body["lastname"];
-			var firstname = request.body["firstname"];
-			var title = request.body["title"];
-			
-			this.lecturer.create(username, password, lastname, firstname, title, function(createError, createResult) {
-				this.responseHandler.handleResponse(createError, createResult, request, response);
-			});
-		};
-		
-		LecturerController.prototype.deleteLecturer = function(request, response) {
-			console.log("Deleting an existing lecturer from the system...");
-			
-			var username = request.params["username"];
-			this.lecturer.deleteLecturer(username, function(deleteError, deleteResult){
-				this.responseHandler.handleResponse(deleteError, deleteResult, request, response);
-			});
-		};
+		this.lecturer = new Lecturer(27017, "localhost", io);
+		this.responseHandler = ResponseHandler.getInstance();
 	}
+
+	LecturerController.prototype.authenticateLecturer = function(request, response) {
+		console.log("Lecturer logging into the system...");
+			
+		var lecturerUsername = request.body.username;
+		var lecturerPassword = request.body.password;
+
+		this.lecturer.authenticate(lecturerUsername, lecturerPassword, function(authenticationError, authenticationResult) {
+			this.responseHandler.handleResponse(authenticationError, authenticationResult, request, response);
+		});
+	};
+
+	LecturerController.prototype.createLecturer = function(request, response) {
+		console.log("Creating a new lecturer into the system...");
+			
+		var username = request.body.username;
+		var password = request.body.password;
+		var lastname = request.body.lastname;
+		var firstname = request.body.firstname;
+		var title = request.body.title;
+			
+		this.lecturer.create(username, password, lastname, firstname, title, function(createError, createResult) {
+			this.responseHandler.handleResponse(createError, createResult, request, response);
+		});
+	};
+
+	LecturerController.prototype.deleteLecturer = function(request, response) {
+		console.log("Deleting an existing lecturer from the system...");
+			
+		var username = request.params.username;
+
+		this.lecturer.deleteLecturer(username, function(deleteError, deleteResult){
+			this.responseHandler.handleResponse(deleteError, deleteResult, request, response);
+		});
+	};
+
 	return LecturerController;
 })();
